ae04df43cdece8ba2dcc954be399279306a13ac1
Jason Costello <jjacostello@gmail.com>
2020-05-20 23:13:37 +0100

    DB model->entity yuk

--- db/models.py
+++ db/models.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from sqlalchemy import Column, DateTime, String, Integer, func
 from extensions import db
+from entities import translation
 
 
 @dataclass
@@ -19,3 +20,8 @@ class Translation(db.Model):
     language = Column(String)
     original_text = Column(String)
     translated_text = Column(String)
+
+    def get(self):
+        return translation.Translation(self.language,
+                                       self.original_text,
+                                       self.translated_text)
