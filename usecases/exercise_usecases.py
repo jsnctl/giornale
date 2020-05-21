39269052129841bcd198a141df69224b9cb5c792
Jason Costello <jjacostello@gmail.com>
2020-05-21 23:26:56 +0100

    Added new random exercise gen usecase (still no fuckin clue if I'm even thinking about use cases correctly here)

--- usecases/exercise_usecases.py
+++ usecases/exercise_usecases.py
@@ -6,11 +6,26 @@ controller = TranslationController()
 
 
 def create_exercise(language, no_translations):
-
     exercise = Exercise()
     translations = controller.get_N_translations_by_language(language, no_translations)
 
     for translation in translations:
+        exercise.add_translation(translation)
+
+    return exercise
+
+
+def create_random_exercise(no_translations):
+
+    # TODO: this is total horseshit as its a call per translation
+    #  should be refactored to a single DB call with a list of ids
+
+    exercise = Exercise()
+
+    random_seed = [np.random.randint(0, 100) for _ in np.arange(no_translations)]
+
+    for translation_id in random_seed:
+        translation = controller.get_translation_by_id(translation_id)
         exercise.add_translation(translation)
 
     return exercise
