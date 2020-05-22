a73cdf641e1b9befc367c275502b9ced831d00b3
Jason Costello <jjacostello@gmail.com>
2020-05-23 00:06:52 +0100

    GFY translation API: now taking multi-lingual RSS input

--- giornale.py
+++ giornale.py
@@ -7,18 +7,36 @@ from sqlalchemy.orm import sessionmaker
 
 CREDENTIALS = yaml.load(open("./credentials.yaml", "rb"),
                         Loader=yaml.FullLoader)
-ITALIAN_NEWS_RSS = "https://www.ansa.it/sito/ansait_rss.xml"
+ENGLISH_RSS = "https://voxeurop.eu/en/feed/rss/all.xml"
+ITALIAN_RSS = "https://voxeurop.eu/it/feed/rss/all.xml"
 
 engine = create_engine(CREDENTIALS['database']['db_url'])
 Session = sessionmaker(bind=engine)
 
-feed = feedparser.parse(ITALIAN_NEWS_RSS)
-entries = feed['entries']
+en_feed = feedparser.parse(ENGLISH_RSS)
+it_feed = feedparser.parse(ITALIAN_RSS)
 
-for entry in entries:
+
+def get_image_link(list_of_json):
+    # used as imperfect key of same article cross-language
+    return [json_element['href']
+            for json_element in list_of_json
+            if json_element['type'] == 'image/jpeg'][0]
+
+
+it_pairs = {get_image_link(row['links']): row['title'] for row in it_feed['entries']}
+en_pairs = {get_image_link(row['links']): row['title'] for row in en_feed['entries']}
+
+for key, original_text in it_pairs.items():
     session = Session()
+
+    try:
+        translated_text = en_pairs[key]
+    except KeyError:
+        continue
+
     headline = Translation(language="italian",
-                           original_text=entry['summary'],
-                           translated_text=translate.translate_text(entry['summary']))
+                           original_text=original_text,
+                           translated_text=translated_text)
     session.add(headline)
     session.commit()
